cd1483150867814c9b27f4f65745d24f95c69c55
Kyle Cordes <kyle.cordes@oasisdigital.com>
2022-04-11 18:29:44 -0500

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,21 @@
 import { registerLocaleData } from '@angular/common';
 // load data for any additional locales you may need
+import localeDe from '@angular/common/locales/de'; // Germany
 import localeFr from '@angular/common/locales/fr'; // France
 import localeRu from '@angular/common/locales/ru'; // Russia
-import {
-  DEFAULT_CURRENCY_CODE,
-  LOCALE_ID,
-  NgModule
-} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 // register locale data
+registerLocaleData(localeDe);
 registerLocaleData(localeFr);
 registerLocaleData(localeRu);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  bootstrap: [AppComponent],
-  providers: [
-    // Localized pipes use these injected values, unless overridden
-    { provide: LOCALE_ID, useValue: 'fr-FR' }, // default is en-US
-    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' } // default is USD
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
