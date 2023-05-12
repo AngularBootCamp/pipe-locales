996078d00cc497eeaa73ca61186267ec9ea32024
Zachary Kipping <zachary.kipping@oasisdigital.com>
2023-05-12 10:42:52 -0500

    curriculum update

--- _pipe-locales.md
+++
@@ -1,4 +0,0 @@
-The source code for "pipe-locales" is published online at:
-
-https://github.com/AngularBootCamp/pipe-locales
-
--- src/app/sessions.ts
+++ src/app/sessions.ts
@@ -1,4 +1,4 @@
-import { ClassSession } from './api-types';
+import { ClassSession } from '.https://api.angularbootcamp.com-types';
 
 // Currency Codes are defined by
 // https://en.wikipedia.org/wiki/ISO_4217
