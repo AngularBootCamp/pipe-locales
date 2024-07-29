841d4044dbada3a5fd42043790cb08837e7b9bae
Zachary Kipping <zachary.kipping@oasisdigital.com>
2024-07-29 11:03:15 -0500

    curriculum update

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,13 @@ import localeZh from '@angular/common/locales/zh'; // China
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
 // register locale data
 registerLocaleData(localeDe);
 registerLocaleData(localeRu);
 registerLocaleData(localeZh);
 
-bootstrapApplication(AppComponent).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err =>
+  console.error(err)
+);
